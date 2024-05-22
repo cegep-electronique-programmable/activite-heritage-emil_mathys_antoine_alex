cf466225207acbb7b27e6f61b785a605c24d71f4
PlamEmil <159059742+PlamEmil@users.noreply.github.com>
2024-05-22 14:06:49 -0400

    Update ActiviteHeritage.cpp

--- ActiviteHeritage.cpp
+++ ActiviteHeritage.cpp
@@ -2,32 +2,31 @@
 using namespace std;
 
 class etudiant {
-    double numeroMatricule;
+    
     int moyenne;
-	
+protected:
+    int numeroMatricule;
 public:
-    etudiant(double);
-    double getMatricule();
+    void setMatricule(int);
+    int getMatricule();
     void setMoyenne(int);
     int getMoyenne();
 };
 
 
-etudiant::etudiant(double nouveauMatricule){
-    //Le numÃ©rod de matricule doit nÃ©cessairement avoir 6 chiffres
-    if ((nouveauMatricule >= 100000) && (nouveauMatricule <= 999999)){
+void etudiant::setMatricule(int nouveauMatricule){
+    //Le numÃ©rod de matricule doit nÃ©cessairement avoir 7 chiffres
+    if ((nouveauMatricule >= 100000) && (nouveauMatricule <= 9999999)){
         this->numeroMatricule = nouveauMatricule;    
     }
     else{
         this->numeroMatricule = 0;
     }
-    
 }
 
-double etudiant::getMatricule(void){
-    return (this->moyenne);
+int etudiant::getMatricule(void){
+    return (this->numeroMatricule);
 }
-
 
 void etudiant::setMoyenne(int nouvelleMoyenne){
     if (nouvelleMoyenne <= 100){
@@ -38,17 +37,43 @@ void etudiant::setMoyenne(int nouvelleMoyenne){
     }
 }
 
-
 int etudiant::getMoyenne(void){
-    return (this->numeroMatricule);
+    return (this->moyenne);
 }
+
+class etudiantMusique : public etudiant{
+    int nbrInstrumentsJoue;
     
+    public : 
+    void setNbrInstrumentsJoues (int nb){
+        nbrInstrumentsJoue = nb;
+    }
+    int getNbrInstrumentsJoues (void){
+        return nbrInstrumentsJoue;
+    }
+    void setMatricule (int nouveauMatricule){
+        if ((nouveauMatricule >= 2000000) && (nouveauMatricule <= 2999999)){
+            numeroMatricule = nouveauMatricule;    
+        }
+        else{
+            numeroMatricule = 9999999;
+        }
+    }
+};
 
 int main() {
-    printf("Hello world !\n\r\n\r");
-    etudiant etudiant147(147);
-    etudiant147.setMoyenne(100);
-    printf("Etudiant147, matricule # %d, moyenne %d", etudiant147.getMatricule(), etudiant147.getMoyenne());
+    int numeroMatriculeTmp;
+    int moyenneTmp;
     
+    printf("Hello world !\n\n");
+    
+    etudiantMusique etudiant1;
+    etudiant1.setMatricule(2039447);
+    etudiant1.setMoyenne(80);
+    etudiant1.setNbrInstrumentsJoues(5);
+    numeroMatriculeTmp = etudiant1.getMatricule();
+    moyenneTmp = etudiant1.getMoyenne(); 
+    printf("Etudiant1, matricule # %d, moyenne %d\n", numeroMatriculeTmp, moyenneTmp);
+    printf("Nb instruments : %d", etudiant1.getNbrInstrumentsJoues());
     return 0;
-} 
+}
